1f402a5788998da5ca2a170d8db8d0f26e6cbc84
Bartłomiej Fatyga <bartlomiej254@wp.pl>
2018-07-16 22:12:34 +0200

    test

--- TEST.py
+++ TEST.py
@@ -5,8 +5,4 @@ print('test4')
 print("test5")
 print('test6')
 print('Wojtek to super kolega')
-<<<<<<< HEAD
 print('bartek fatyga to koleżka najlepszy iksd')
-=======
-print('jest git')
->>>>>>> 2aa86536839720a2d2aa417b009af06fdc0da574
